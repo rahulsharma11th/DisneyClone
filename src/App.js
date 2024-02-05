6e01d92f474a844597ee7675251033558c83422f
Rahul sharma <48442696+rahulsharma11th@users.noreply.github.com>
2024-02-05 22:03:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,64 @@ function App() {
 }
 
 export default App;
+
+
+
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { MSGraphClient } from '@microsoft/sp-http';
+
+import UserReporteeInfo, { IUserReporteeInfoProps } from './components/UserReporteeInfo';
+
+export interface IYourWebPartProps {
+  userId: string;
+}
+
+export default class YourWebPart extends BaseClientSideWebPart<IYourWebPartProps> {
+  public render(): void {
+    this.context.msGraphClientFactory
+      .getClient()
+      .then((graphClient: MSGraphClient) => {
+        const element: React.ReactElement<IUserReporteeInfoProps> = React.createElement(
+          UserReporteeInfo,
+          {
+            graphClient: graphClient,
+            userId: this.properties.userId
+          }
+        );
+
+        ReactDom.render(element, this.domElement);
+      });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: { description: "Web Part Configuration" },
+          groups: [
+            {
+              groupName: "Settings",
+              groupFields: [
+                PropertyPaneTextField('userId', {
+                  label: "User ID"
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
+
